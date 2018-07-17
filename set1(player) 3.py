89467df001ad06be151e92b97c5c408a8177db20
SharanRagesh <40879679+SharanRagesh@users.noreply.github.com>
2018-07-17 11:39:21 +0530

    Update set1(player) 3.py

--- set1(player) 3.py
+++ set1(player) 3.py
@@ -1,7 +1,7 @@
-n=int(input())
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-print(rev)
+num=int(input())
+reverse=0
+while(num>0):
+    digit=num%10
+    reverse=reverse*10+digit
+    num=num//10
+print(reverse)
