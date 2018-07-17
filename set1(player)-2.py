ad85a94f968401d567789308f19f9aec98777b77
SharanRagesh <40879679+SharanRagesh@users.noreply.github.com>
2018-07-17 11:28:11 +0530

    Create set1(player)-2.py

---
+++ set1(player)-2.py
@@ -0,0 +1,10 @@
+num = int(input("enter the number"))
+factorial = 1
+if num < 0:
+   print("Factorial of negative number does not exist ")
+elif num == 0:
+   print("1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print(factorial
