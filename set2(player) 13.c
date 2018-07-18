be257cdb31720b060619bdf7b796edd29a70e8be
SharanRagesh <40879679+SharanRagesh@users.noreply.github.com>
2018-07-18 11:30:01 +0530

    Create set2(player) 13.c

---
+++ set2(player) 13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+ int n,t=0,r,q;
+ scanf("%d",&n);
+ q=n;
+ do
+ {
+      r=q%10;
+      t += r*r;
+      q=q/10;
+ }
+ while(q%10!=0);
+ printf("%d",t);
+}
